38256f65b365165028212e45beca9a4850aa9bc8
Darshan G N <darshu0535@gmail.com>
2024-12-14 15:47:25 +0530

    sixth commit

---
+++ Face_detection/src/test/java/com/Face_detection/Face_detection/FaceDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Face_detection.Face_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

84b4c78aacb6c25cd8ba788a29a315d4d5e8ac01
Darshan G N <darshu0535@gmail.com>
2024-12-14 15:18:40 +0530

    initial commit

---
+++ Face_detection/src/main/java/com/Face_detection/Face_detection/FaceDetectionApplication.java
@@ -0,0 +1,15 @@
+package com.Face_detection.Face_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceDetectionApplication {
+
+	public static void main(String[] args) {
+//		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		SpringApplication.run(FaceDetectionApplication.class, args);
+
+	}
+
+}
